774a5fbce26d1e36c88511bb67bd78a6c8746f14
OverLord <overlord@localhost.localdomain>
2020-06-04 11:28:39 +0700

    homework:

---
+++ app/Http/Controllers/LanguageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class LanguageController extends Controller
+{
+    public function changeLanguage($language)
+    {
+        // Tạo session và gán giá trị ngôn ngữ được chọn từ view
+        Session::put('language', $language);
+
+        return redirect()->back();
+    }
+}
